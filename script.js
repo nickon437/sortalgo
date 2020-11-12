6da908529247558373a18acb30287697d90dd3e3
Nick On <nick.on437@gmail.com>
2020-11-12 19:33:07 +1100

    Refactor - Change to OOP

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-const DEFAULT_UNSORTED_CHARS = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-let unsortedHtml;
+import CharacterList from './CharacterList.js';
+
 let multiplier = 1;
+const charList = new CharacterList();
+console.log(charList);
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const createUnsortedList = () => {
-    const SORTED_COLORS = [
-        { character: 'A', color: '#c33f3b' },
-        { character: 'B', color: '#c85141' },
-        { character: 'C', color: '#d07447' },
-        { character: 'D', color: '#d79b4f' },
-        { character: 'E', color: '#dbb454' },
-        { character: 'F', color: '#d9c95a' },
-        { character: 'G', color: '#d9db5c' },
-        { character: 'H', color: '#cadd59' },
-        { character: 'I', color: '#b4dc52' },
-        { character: 'J', color: '#93cf4d' },
-        { character: 'K', color: '#7dc346' },
-        { character: 'L', color: '#5db947' },
-        { character: 'M', color: '#48aa4a' },
-        { character: 'N', color: '#40a24d' },
-        { character: 'O', color: '#429f59' },
-        { character: 'P', color: '#439a64' },
-        { character: 'Q', color: '#429774' },
-        { character: 'R', color: '#439286' },
-        { character: 'S', color: '#448d93' },
-        { character: 'T', color: '#438aa3' },
-        { character: 'U', color: '#4786ae' },
-        { character: 'V', color: '#607ab6' },
-        { character: 'W', color: '#7371b6' },
-        { character: 'X', color: '#8a6cb5' },
-        { character: 'Y', color: '#a265b6' },
-        { character: 'Z', color: '#b060b2' },
-    ]
-
-    unsortedHtml = DEFAULT_UNSORTED_CHARS.map((character) => {
-        const { color } = SORTED_COLORS.find((element) => element.character === character);
-        return `<div id="sel-sort-${character}" class="element" style="background-color: ${color}">${character}</div>`
-    });
-
-    $(".illustration-elements").html(unsortedHtml.join(''));
-}
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getMidPoint = (character) => {
     const margin = $(".illustration-elements").position().left;
@@ -53,9 +15,8 @@ const getMidPoint = (character) => {
 }
 
 async function selectionSort() {
-    const localQuery = [...DEFAULT_UNSORTED_CHARS];
-    let minIndex;
-    
+    // const localQuery = [...DEFAULT_UNSORTED_CHARS];
+
     const FACTOR = 500;
     let waitTime = FACTOR * multiplier;
 
@@ -63,9 +24,8 @@ async function selectionSort() {
     $('button').prop('disabled', true);
 
     const getIdByIndex = (index) => {
-        return `#sel-sort-${localQuery[index]}`
+        return `#sel-sort-${charList.getChar(index)}`
     }
-
     /**
      * To create a seemless swapping effect, 
      * 
@@ -81,80 +41,71 @@ async function selectionSort() {
 
         await sleep(waitTime);
 
-        // DATA
-        let temp = localQuery[x];
-        localQuery[x] = localQuery[y];
-        localQuery[y] = temp;
-        
         // Swapp background and innerText for the visual fading effect
-        temp = $(xId)[0].style.backgroundColor;
+        let temp = $(xId)[0].style.backgroundColor;
         $(xId)[0].style.backgroundColor = $(yId)[0].style.backgroundColor;
         $(yId)[0].style.backgroundColor = temp;
-        
+
         temp = $(xId).text();
         $(xId).text($(yId).text());
         $(yId).text(temp);
-        
+
         $(xId).fadeTo(waitTime, 1);
         $(yId).fadeTo(waitTime, 1);
         await sleep(waitTime);
 
         // Swapping the div elements because swapping background and innerText is only for the visual effect but the ID is still stay the same.
-        temp = unsortedHtml[x];
-        unsortedHtml[x] = unsortedHtml[y];
-        unsortedHtml[y] = temp;
-        $(".illustration-elements").html(unsortedHtml.join(''));
+        charList.swapElements(x, y);
+        $(".illustration-elements").html(charList.getHtmlArray().join(''));
     };
 
-    for (let i = 0; i < localQuery.length - 1; i++) {
-        minIndex = i;
-        $('.line-min').css('left', getMidPoint(localQuery[minIndex]));
-        $('.line-i').css('left', getMidPoint(localQuery[minIndex]));
-        
+    for (let i = 0; i < charList.getLength() - 1; i++) {
+        let minIndex = i;
+        const midPosI = getMidPoint(charList.getChar(minIndex));
+        $('.line-min').css('left', midPosI);
+        $('.line-i').css('left', midPosI);
         await sleep(waitTime);
-        for (let j = i + 1; j < localQuery.length; j++) {
-            
+
+        for (let j = i + 1; j < charList.getLength(); j++) {
             // Update speed        
             waitTime = FACTOR * multiplier;
 
             $('.line-j').fadeIn();
-            $('.line-j').css('left', getMidPoint(localQuery[j]));
+            $('.line-j').css('left', getMidPoint(charList.getChar(j)));
             await sleep(waitTime);
 
-            if (localQuery[j] < localQuery[minIndex]) {
+            if (charList.getChar(j) < charList.getChar(minIndex)) {
                 $('.line-j').css('background-color', 'red');
                 await sleep(waitTime);
 
                 minIndex = j;
-                $('.line-min').css('left', getMidPoint(localQuery[minIndex]));
+                $('.line-min').css('left', getMidPoint(charList.getChar(minIndex)));
                 await sleep(waitTime);
 
                 $('.line-j').css('background-color', '');
             }
         }
 
-
         if (minIndex !== i) {
-            // Swap the found minimum element with the first element
-            await swapContent(minIndex, i);
+            await swapContent(minIndex, i); // Swap the found minimum element with the first element
         }
 
-        $('.sorted-marker').css('left', `calc(${getMidPoint(localQuery[i + 1])} - 5.5rem)`);
-        
+        $('.sorted-marker').css('left', `calc(${getMidPoint(charList.getChar(i + 1))} - 5.5rem)`);
     }
-    
+
     $('.illustration-line').fadeOut();
     $('button').fadeIn();
-    $('#sort-btn').hide();
     $('button').prop('disabled', false);
+    $('#sort-btn').hide();
 }
 
 const initElementList = () => {
     $('#reset-btn').hide();
     $('#sort-btn').fadeIn();
 
-    createUnsortedList();
-    const startingPos = getMidPoint(DEFAULT_UNSORTED_CHARS[0]);
+    charList.shuffle();
+
+    const startingPos = getMidPoint(charList.getChar(0));
     $('.sorted-marker').css('left', `calc(${startingPos} - 5.5rem)`);
     $('.line-min').css('left', startingPos);
     $('.line-i').css('left', startingPos);
